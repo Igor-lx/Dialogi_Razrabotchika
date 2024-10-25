c73401ef3deb22e6f735329d69db024cfae91474
Igor B <lorax.k@yandex.ru>
2024-10-25 13:02:34 +0200

    v0.2

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,6 +1,42 @@
+function initTheme() {
+  const savedTheme = localStorage.getItem("userTheme");
+
+  if (savedTheme) {
+    document.body.classList.toggle("dark_theme", savedTheme === "dark");
+  } else {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    document.body.classList.toggle("dark_theme", prefersDark);
+
+    localStorage.setItem("userTheme", prefersDark ? "dark" : "light");
+  }
+}
+
+initTheme();
+
+/* --------------------------------------------------------------------------------------- */
+
 function toggleTheme() {
   document.body.classList.toggle("dark_theme");
-}
-const themeSwitcherButton = document.querySelector(".theme-toggle_button");
 
+  const currentTheme = document.body.classList.contains("dark_theme")
+    ? "dark"
+    : "light";
+  localStorage.setItem("userTheme", currentTheme);
+}
+
+/* --------------------------------------------------------------------------------------- */
+
+const themeSwitcherButton = document.querySelector(".theme-toggle_button");
 themeSwitcherButton.addEventListener("click", toggleTheme);
+
+/* --------------------------------------------------------------------------------------- */
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
+    const newTheme = e.matches ? "dark" : "light";
+    document.body.classList.toggle("dark_theme", newTheme === "dark");
+    localStorage.setItem("userTheme", newTheme);
+  });
