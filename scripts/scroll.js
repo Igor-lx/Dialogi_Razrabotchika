4a63ccf89e8d21c326a6f1092af52d5e1f774763
Igor B <lorax.k@yandex.ru>
2024-10-26 19:52:26 +0200

    v0.6

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,20 +1,25 @@
-window.onscroll = function () {
-  const upButton = document.getElementById("up-button");
-  const copyright = document.getElementById("copyright");
+function showButtonsOnscroll() {
+  const copyrightButton = document.querySelector(".copyright_button");
+  const upButton = document.querySelector(".up-button");
 
   if (window.innerWidth > 768) {
     if (
       document.body.scrollTop > 400 ||
       document.documentElement.scrollTop > 400
     ) {
+      copyrightButton.style.display = "flex";
       upButton.style.display = "flex";
-      copyright.style.display = "flex";
     } else {
+      copyrightButton.style.display = "none";
       upButton.style.display = "none";
-      copyright.style.display = "none";
     }
   } else {
+    copyrightButton.style.display = "none";
     upButton.style.display = "none";
-    copyright.style.display = "none";
   }
-};
+}
+
+window.onscroll = showButtonsOnscroll;
+window.onresize = showButtonsOnscroll;
+
+showButtonsOnscroll();
