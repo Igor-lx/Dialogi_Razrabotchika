ea14b937df6ad4acf5c253ba4f19ace92f41136d
Igor B <lorax.k@yandex.ru>
2024-10-26 23:39:52 +0200

    v0.7+

---
+++ scripts/buttonAttributes.js
@@ -0,0 +1,48 @@
+const buttonData = [
+  {
+    name: "GALAXY button",
+
+    type: "button",
+    buttonClass: "wrapper-button galaxy_button",
+    tabindex: "-1",
+    "aria-hidden": "true",
+    svgClass: "aside_left_icon",
+    viewBox: "0 0 70 70",
+    svgHref: "./images/svg/galaxy.svg#galaxy",
+  },
+
+  {
+    name: "THEME-TOGGLE button",
+
+    type: "button",
+    buttonClass: "wrapper-button theme-toggle_button",
+    "aria-label": "переключить тему",
+    svgClass: "aside_right_icon theme_icon",
+    viewBox: "0 0 24 24",
+    svgHref: "./images/svg/themes.svg#auto",
+  },
+
+  {
+    name: "COPYRIGHT button",
+
+    type: "button",
+    buttonClass: "wrapper-button copyright_button",
+    tabindex: "-1",
+    "aria-hidden": "true",
+    svgClass: "aside_left_icon",
+    viewBox: "0 0 36 32",
+    svgHref: "./images/svg/copyright.svg#copyright",
+  },
+
+  {
+    name: "UP-BUTTON",
+
+    type: "button",
+    buttonClass: "wrapper-button up-button",
+    onclick: "location.href='#top'",
+    "aria-label": "навех страницы",
+    svgClass: "aside_right_icon up_icon",
+    viewBox: "0 0 24 24",
+    svgHref: "./images/svg/arrow-up.svg#up",
+  },
+];
---
+++ scripts/createElements.js
@@ -0,0 +1,39 @@
+function createButton(ButtonDataArray) {
+  const buttonItem = document
+    .querySelector("#button_template")
+    .content.cloneNode(true);
+
+  const button = buttonItem.querySelector("button");
+  const svg = buttonItem.querySelector("svg");
+  const svgUse = svg.querySelector("use");
+
+  button.setAttribute("type", ButtonDataArray.type);
+  button.className = ButtonDataArray.buttonClass;
+
+  if (ButtonDataArray.onclick) {
+    button.setAttribute("onclick", ButtonDataArray.onclick);
+  }
+
+  if (ButtonDataArray.tabindex) {
+    button.setAttribute("tabindex", ButtonDataArray.tabindex);
+  }
+  if (ButtonDataArray["aria-hidden"]) {
+    button.setAttribute("aria-hidden", ButtonDataArray["aria-hidden"]);
+  }
+  if (ButtonDataArray["aria-label"]) {
+    button.setAttribute("aria-label", ButtonDataArray["aria-label"]);
+  }
+
+  svg.setAttribute("class", ButtonDataArray.svgClass);
+  svg.setAttribute("viewBox", ButtonDataArray.viewBox);
+
+  svgUse.setAttribute("href", ButtonDataArray.svgHref);
+
+  return buttonItem;
+}
+
+const buttonPlace = document.querySelector(".aside_area");
+
+buttonData.forEach(function (index) {
+  buttonPlace.append(createButton(index));
+});
